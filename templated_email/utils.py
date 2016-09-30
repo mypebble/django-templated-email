4d9ae9a8041d56b0494fd94d7a14fead82a2e536
Scott Walton <scott.walton@mypebble.co.uk>
2016-09-30 15:14:50 +0100

    Change xrange for range for py3

--- templated_email/utils.py
+++ templated_email/utils.py
@@ -12,7 +12,7 @@ def _iter_nodes(template, context, name, block_lookups):
     for node in template.template.nodelist:
         if isinstance(node, BlockNode) and node.name == name:
             # Rudimentary handling of extended templates, for issue #3
-            for i in xrange(len(node.nodelist)):
+            for i in range(len(node.nodelist)):
                 n = node.nodelist[i]
                 if isinstance(n, BlockNode) and n.name in block_lookups:
                     node.nodelist[i] = block_lookups[n.name]
